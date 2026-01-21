5c9523adf79b9f47ef28f50e65ffe9818a2a6d1a
Felipe Pessin <fhpessin@gmail.com>
2026-01-21 14:01:43 -0300

    Update test_restore_names.py

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -7,20 +7,43 @@ from app.main import restore_names
     [
         (
             [
-                {"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"},
-                {"last_name": "Adams", "full_name": "Mike Adams"},
+                {
+                    "first_name": None,
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy"
+                },
+                {
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams"
+                },
             ],
             [
-                {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"},
-                {"first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"},
+                {
+                    "first_name": "Jack",
+                    "last_name": "Holy",
+                    "full_name": "Jack Holy"
+                },
+                {
+                    "first_name": "Mike",
+                    "last_name": "Adams",
+                    "full_name": "Mike Adams"
+                },
             ],
         ),
         (
             [
-                {"first_name": "Anna", "last_name": "Smith", "full_name": "Anna Smith"},
+                {
+                    "first_name": "Anna",
+                    "last_name": "Smith",
+                    "full_name": "Anna Smith"
+                },
             ],
             [
-                {"first_name": "Anna", "last_name": "Smith", "full_name": "Anna Smith"},
+                {
+                    "first_name": "Anna",
+                    "last_name": "Smith",
+                    "full_name": "Anna Smith"
+                },
             ],
         ),
         (
